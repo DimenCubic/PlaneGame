b6a3729307ef13852bddf36ca00d35222402341b
dimen10 <dimen10@student.ubc.ca>
2025-05-05 15:34:10 -0700

    添加项目文件。

---
+++ PLaneGame/PLaneGame.cpp
@@ -0,0 +1,36 @@
+﻿ 
+
+#include<stdio.h>
+#include<graphics.h>
+
+IMAGE bk;
+IMAGE img_plane[2]; // Plane 
+
+void loadImg() {
+	// Initilization picture.
+	loadimage(&bk, "material\\Background.jpg");
+	
+	loadimage(&img_plane[0], "material\\Plane.jpg");
+	loadimage(&img_plane[1], "material\\Enemy.jpg");
+}
+
+void gameDraw() {
+	
+	// Initilization
+	loadImg();
+
+
+}
+
+
+int main()
+{
+	// Game window
+	initgraph(480, 850);
+
+	while (1);
+
+	return 0;
+}
+
+ 
